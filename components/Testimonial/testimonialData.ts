e91f807dbb896feddc56824c6ba3aa7147dbc22f
NoelOsiro <noel.osiro@azubiafrica.org>
2024-07-12 23:02:40 +0300

    programs

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -1,7 +1,7 @@
 import image1 from "@/public/images/user/user-01.png";
 import image2 from "@/public/images/user/user-03.jpeg";
 import image3 from "@/public/images/user/user-04.jpeg";
-import image4 from "@/public/images/user/user-05.jpeg";
+import image4 from "@/public/images/user/user-02.png";
 import { Testimonial } from "@/types/testimonial";
 
 export const testimonialData: Testimonial[] = [
@@ -17,24 +17,24 @@ export const testimonialData: Testimonial[] = [
     id: 2,
     name: "Symprose  Okecha",
     designation: "Proud home owner, Kibera-Kenya",
-    image: image2,
+    image: image4,
     content:
-      "I am grateful for the support and education provided by Brainstorm Centre. The widow support group has helped my business and  now I managed to build  my house.",
+      "I am grateful for the support and education provided by Brainstorm Centre. The widow support group has helped my business and  now I managed to build  my house. I am thankful for the work they do and the difference they make in our community.",
   },
   {
     id: 3,
     name: "Pamela Nabwire",
     designation: "Chairlady Widows  Group, Kibera-Kenya",
-    image: image3,
+    image: image2,
     content:
-      "As a local businesswoman, I have seen the positive changes brought about by the Brainstorm Junior Children's Centre. The support and education provided to the children have a ripple effect on the community, improving lives and fostering growth.",
+      "As a local businesswoman, I have seen the positive changes brought about by the Brainstorm Junior Children's Centre. The support and education provided to the children have a ripple effect on the community, improving lives and fostering growth. ",
   },
   {
     id: 4,
     name: "Siprina Awiti",
     designation: "Widower, Kibera-Kenya",
-    image: image4,
+    image: image3,
     content:
-      `I trained on business management, financial litracy etc. At the moment I am running my business that I started in 2019 `
+      `I trained on business management, financial litracy etc. At the moment I am running my business that I started in 2019. I feel empowered and I am able to support my family. `
   },
 ];
